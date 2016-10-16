208c2b794ac75d8032b2ee931f176f7714432903
fundamental <mark.d.mccurry@gmail.com>
2016-10-16 14:47:47 -0400

    Re-Enable Eagar Message Sending
    I think this was previously disabled due to packet loss, but
    that should be mitigated with the retry code (I hope).

--- src/gem.c
+++ src/gem.c
@@ -269,7 +269,7 @@ static void do_send(bridge_t *br, char *buffer, unsigned len)
     struct sockaddr_in send_addr;
     uv_ip4_addr(br->address, br->port, &send_addr);
     uv_udp_send(&request->send_req, &br->socket, &buf, 1, (const struct sockaddr *)&send_addr, send_cb);
-    //uv_run(br->loop, UV_RUN_NOWAIT);
+    uv_run(br->loop, UV_RUN_NOWAIT);
 }
 
 void osc_request(bridge_t *br, const char *path)
